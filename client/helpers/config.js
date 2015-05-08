b4fa9a1078104db8a3688b320b3f262faf271b25
Marco Giannone <marco.giannone@gmail.com>
2015-05-08 13:09:07 +0200

    Added user creation, restyled header

---
+++ client/helpers/config.js
@@ -0,0 +1,7 @@
+// In order to configure the accounts UI to use 
+// usernames instead of email address
+//TODO: check for sending email smtp (reset password)
+// At the bottom of the client code
+Accounts.ui.config({
+  passwordSignupFields: 'USERNAME_ONLY'
+});
